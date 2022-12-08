7f7d20226dd66a2b256ff05561651105d9ea369b
KrisN9 <kristinank992@gmail.com>
2022-12-08 19:34:49 +0000

    ejercicio para entregar

--- src/models.py
+++ src/models.py
@@ -11,28 +11,32 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    firstname = Column(String(250), nullable=False)
-    lastname = Column(String(250), nullable=False)
+    firstname = Column(String(100), nullable=False)
+    lastname = Column(String(100), nullable=False)
     email = Column(String(250), nullable=False)
 
 class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    planet_name = Column(String(250))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    planet_name = Column(String(50))
+    climate = Column(String(30))
+    population = Column(Integer, nullable=True)
 
 class Character(Base):
     __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    birth_date = Column(String(250), nullable=True)
-    gender = Column(String(250), nullable=False)
+    name = Column(String(100), nullable=False)
+    birth_date = Column(String(70), nullable=True)
+    gender = Column(String(10), nullable=False)
     affilliation = Column(String(250), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     planet_id = Column(Integer, ForeignKey('planet.id'))
-    user = relationship(User)
-    planet = relationship(Planet)
+    character_id = Column(Integer, ForeignKey('character.id'))
 
     def to_dict(self):
         return {}
